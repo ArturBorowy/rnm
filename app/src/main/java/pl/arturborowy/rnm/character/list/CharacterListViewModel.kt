0c722d3290afc561e4e3e40550d99513c2ede925
arturborowy <a.borowy@mobter.com>
2020-07-28 15:24:10 +0200

    Present characters data on fragment.

--- app/src/main/java/pl/arturborowy/rnm/character/list/CharacterListViewModel.kt
+++ app/src/main/java/pl/arturborowy/rnm/character/list/CharacterListViewModel.kt
@@ -1,12 +1,17 @@
 package pl.arturborowy.rnm.character.list
 
+import androidx.databinding.ObservableArrayList
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.subscribeBy
+import me.tatarka.bindingcollectionadapter2.ItemBinding
+import pl.arturborowy.rnm.BR
+import pl.arturborowy.rnm.R
 import pl.arturborowy.rnm.base.rx.SchedulerProvider
 import pl.arturborowy.rnm.base.rx.setSchedulers
 import pl.arturborowy.rnm.base.ui.viewmodel.FragmentViewModel
 import pl.arturborowy.rnm.base.ui.viewmodel.RxJavaSubscriber
 import pl.arturborowy.rnm.domain.characters.CharactersRemoteRepository
+import pl.arturborowy.rnm.domain.characters.model.CharacterDetailsEntity
 import timber.log.Timber
 
 class CharacterListViewModel(
@@ -15,15 +20,23 @@ class CharacterListViewModel(
     private val schedulerProvider: SchedulerProvider
 ) : FragmentViewModel(), RxJavaSubscriber {
 
+    val characters = ObservableArrayList<CharacterDetailsEntity>()
+    val charactersBinding =
+        ItemBinding.of<CharacterDetailsEntity>(BR.vm, R.layout.item_character)
+
     override fun onViewCreated() {
+        fetchCharacters()
+    }
+
+    private fun fetchCharacters() {
         charactersRemoteRepository.getCharacters()
             .setSchedulers(schedulerProvider)
             .subscribeBy(
-                onSuccess = {Timber.e(it.toString())},
-                onError = {
-                    //TODO
-                    Timber.w(it)
-                }
+                onSuccess = {
+                    characters.clear()
+                    characters.addAll(it.characters)
+                },
+                onError = { Timber.w(it) }
             ).addToSubs()
     }
 
